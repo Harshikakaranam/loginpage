956a62a1942c95ce7d9081e4fae8985fe92c6e52
Harshikakaranam <129184927+Harshikakaranam@users.noreply.github.com>
2023-04-03 20:39:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+
+import './App.css';
+
+function App() {
+  return (
+  <>
+    <div className="upper">
+      <div className="container">
+      <div className="card">
+       <h1>Login</h1>
+       <div className="img">
+         <img src="logo.png" alt=""/>
+       </div>
+       
+       <form action="">
+         <input type="text" placeholder="Username"/>
+         <input type="password" placeholder="Password"/>
+         <div className="img">
+           <input type="submit" placeholder="submit" className="submit"/>
+         </div>
+       </form>
+      </div>
+     
+      </div>
+    </div> 
+    
+    <div className="lower">
+       <div className="curve">
+   
+       </div>
+    </div> 
+
+    </>
+  );
+}
+
+export default App;
